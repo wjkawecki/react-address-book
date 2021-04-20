ec1d9d2c9d2bf4dd2214b818feeba52bb3f18d7d
Wojciech Kawecki <wojciech.kawecki@unic.com>
2021-04-20 21:02:42 +0200

    fine-tune ESlint ans TS configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
   },
   rules: {
     'linebreak-style': 'off',
+    'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'prettier/prettier': [
       'error',
